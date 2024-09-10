ad03a7d33d37a346de6a06890028d3bc6d1e7688
XWippie <xander.waeghe@gmail.com>
2024-09-10 16:03:26 +0200

    added navbar and bg

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded with JavaScript");
+  startNav();
 });
+
+const startNav = function () {
+  let toggleTrigger = document.querySelectorAll(".js-toggle-nav");
+
+  for (let i = 0; i < toggleTrigger.length; i++) {
+    toggleTrigger[i].addEventListener("click", function () {
+      document.querySelector("body").classList.toggle("has-mobile-nav");
+    });
+  }
+
+  const links = document.querySelectorAll(".js-nav-link");
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function () {
+      changeIsSelected(links, links[i]);
+    });
+  }
+};
+
+const changeIsSelected = function (links, selectedLink) {
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.remove("is-selected");
+  }
+
+  let dataValue = selectedLink.getAttribute("data-id");
+  let elements = document.querySelectorAll("[data-id=" + dataValue + "]");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add("is-selected");
+  }
+};
